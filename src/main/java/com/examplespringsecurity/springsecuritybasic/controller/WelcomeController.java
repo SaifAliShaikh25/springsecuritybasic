5b1632217cfb26cd7f87c323e799b06403db7fdb
Saif <saifali.shaikh2509@gmail.com>
2023-12-07 19:57:40 +0530

    first basic commit

---
+++ src/main/java/com/examplespringsecurity/springsecuritybasic/controller/WelcomeController.java
@@ -0,0 +1,15 @@
+package com.examplespringsecurity.springsecuritybasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class WelcomeController {
+
+    @GetMapping("welcome")
+    public String getWelcomeMessage(){
+        return "Welcome to Spring Application with security";
+    }
+}
